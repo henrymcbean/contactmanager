14931fe58f7d8ce8d72fcbb9f840d28ab8266eca
Henry McBean <henry_mcbean@hotmail.com>
2019-11-10 19:05:44 +0000

    Completed Events in React.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-        <Contacts />
+          <Contacts />
         </div>
       </div>
     );
