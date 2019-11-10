c5648735ce780b09a740b27fe113006bcac98fb2
Henry McBean <henry_mcbean@hotmail.com>
2019-11-10 19:45:18 +0000

    Second example of an event , this time event code is in the render, Entered on the onClick event since it is small amount of code.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,18 +6,15 @@ class Contact extends Component {
     showContactInfo: true
   };
 
-  onShowClick = () => {
-    this.setState({
-      showContactInfo: !this.state.showContactInfo
-    });
-  };
-
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i>
+          {name} <i onClick={() => this.setState({
+            showContactInfo: !this.state.showContactInfo
+          })} 
+          className="fas fa-sort-down"></i>
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
