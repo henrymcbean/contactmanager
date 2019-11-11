6ce08ef5078ea1a231de45356ad8cdfda0d10a57
Henry McBean <henry_mcbean@hotmail.com>
2019-11-11 01:53:12 +0000

    Completed 2. Controlled Components & onChange.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,20 @@ class AddContact extends Component {
     phone: ''
   }
 
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input 
@@ -23,6 +30,7 @@ class AddContact extends Component {
                 form-control-lg"
                 placeholder="Enter Name..."
                 value={name}
+                onChange={this.onChange}
                 />
             </div>
             <div className="form-group">
@@ -34,6 +42,7 @@ class AddContact extends Component {
                 form-control-lg"
                 placeholder="Enter Email..."
                 value={email}
+                onChange={this.onChange}
                 />
             </div>
             <div className="form-group">
@@ -45,6 +54,7 @@ class AddContact extends Component {
                 form-control-lg"
                 placeholder="Enter Phone..."
                 value={phone}
+                onChange={this.onChange}
                 />
             </div>
             <input type="submit" value="Add Contact"
