e940201662be768ebbaba8604aa95d6dc0630095
Henry McBean <henry_mcbean@hotmail.com>
2019-11-12 14:18:59 +0000

    Completed 2. GET Requests, used as an example fetch in browser console https://jsonplaceholder.typicode.com/.  As well as using axios in componentDidMount - context.js.

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,46 +2,54 @@ import React, { Component } from 'react'
 
 class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body: ''
   };
 
   componentDidMount() {                     // Order of most often Used 1
-    console.log('componentDidMount...');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({
+        title: data.title,
+        body: data.body
+      }));
   }
 
-  UNSAFE_componentWillMount() {             // Order of most often Used 2 - Will be deprecated
-    console.log('componentWillMount...');
-  }
+  // UNSAFE_componentWillMount() {             // Order of most often Used 2 - Will be deprecated
+  //   console.log('componentWillMount...');
+  // }
 
-  // Did not work in this component since we don't update
-  componentDidUpdate() {                    // Order of most often Used 3
-    console.log('componentDidUpdate...');
-  }
+  // // Did not work in this component since we don't update
+  // componentDidUpdate() {                    // Order of most often Used 3
+  //   console.log('componentDidUpdate...');
+  // }
 
-  componentWillUpdate() {                   // This will fire before above. - Will be deprecated
-    console.log('componentWillUpdate...');
-  }
+  // componentWillUpdate() {                   // This will fire before above. - Will be deprecated
+  //   console.log('componentWillUpdate...');
+  // }
 
-  componentWillReceiveProps(nextProps, nextState) { // Order of most often Used 5 - Will be deprecated
-    console.log('componentWillReceiveProps...');    // Used in Redux
-  }
+  // componentWillReceiveProps(nextProps, nextState) { // Order of most often Used 5 - Will be deprecated
+  //   console.log('componentWillReceiveProps...');    // Used in Redux
+  // }
 
-    // Replacing componentWillReceiveProps
-  static getDerivedStateFromProps(nextProps, prevState) { // Order of most often Used 6
-    return  {
-      test: 'something'   // You cannot use setState have to return some part of the state.
-    };
-  }
+  //   // Replacing componentWillReceiveProps
+  // static getDerivedStateFromProps(nextProps, prevState) { // Order of most often Used 6
+  //   return  {
+  //     test: 'something'   // You cannot use setState have to return some part of the state.
+  //   };
+  // }
 
-  // New Life Cycle Methods
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUpdate...');   
-  }
+  // // New Life Cycle Methods
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapshotBeforeUpdate...');   
+  // }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     )
   }
